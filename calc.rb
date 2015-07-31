ffdff15723efaac88fba30af779bc5a18f78aa22
ivan <ivangarfdez@gmail.com>
2015-07-31 09:13:57 +0200

    Empezando a programar en Ruby, instrucciones básicas con enteros, floats y texto.

--- calc.rb
+++ calc.rb
@@ -8,9 +8,18 @@ puts 24.0 * 443.0
 puts 5.0 - 8.0
 puts 20000000.0 / 2.0
 puts "\n"
+
 puts "Enteros"
 puts "\n"
 puts 6 + 2
 puts 22345 * 323543
 puts 523 - 8
 puts 90 / 2
+puts "\n"
+
+puts "Texto"
+puts "Hola, mundo!"
+puts "Estoy probando Ruby."
+puts "Que pases un muy buen día"
+puts "Adiós."
+
