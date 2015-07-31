151323bc6a9aa19bb16374a8ceb83b10464275da
ivan <ivangarfdez@gmail.com>
2015-07-31 09:13:57 +0200

    Empezando a programar en Ruby, instrucciones básicas con enteros y floats

---
+++ calc.rb
@@ -0,0 +1,16 @@
+puts "\Empezando a programar en Ruby."
+puts "\n"
+
+puts "Flotantes"
+puts "\n"
+puts 7.0 + 1.0
+puts 24.0 * 443.0
+puts 5.0 - 8.0
+puts 20000000.0 / 2.0
+puts "\n"
+puts "Enteros"
+puts "\n"
+puts 6 + 2
+puts 22345 * 323543
+puts 523 - 8
+puts 90 / 2
