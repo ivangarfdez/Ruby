de3d5190a9cd41ad026ed712e063df5cebc1c064
ivan <ivangarfdez@gmail.com>
2015-08-04 16:31:55 +0200

    Empezando a programar en Ruby, instrucciones básicas con enteros, floats y texto.

---
+++ conversiones.rb
@@ -0,0 +1,16 @@
+class Conversiones
+  #Conversion a String
+  var1=7
+  var2="7"
+
+
+  puts var1.to_s + var2
+
+#Conversion
+  var3 = 7
+  var4 = "7"
+
+  puts var4.to_i+var3
+
+
+end
--- variables.rb
+++ variables.rb
@@ -3,3 +3,9 @@ puts "Variables."
 String = 'Me llamo Iván'
 
 puts "¿Sabes cómo me llamo? " "\n" +String
+
+var1 = 7
+var2 = 8
+
+total = var1*var2
+puts total
